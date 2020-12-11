81c205e226a402ac2033a083e03e0330b3c66330
LeonardoAguilar64 <leonardolloydaguilar64@gmail.com>
2020-12-11 09:13:09 -0800

    Initial commit

---
+++ Keyboards.java
@@ -0,0 +1,54 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+ 
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+ 
+public class Keyboards {
+ 
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Key Listener");
+        Container contentPane = frame.getContentPane();
+        KeyListener listener = new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent event) {
+                printEventInfo("Key Pressed", event);
+            }
+            @Override
+            public void keyReleased(KeyEvent event) {
+                printEventInfo("Key Released", event);
+            }
+            @Override
+            public void keyTyped(KeyEvent event) {
+                printEventInfo("Key Typed", event);
+            }
+            private void printEventInfo(String str, KeyEvent e) {
+                System.out.println(str);
+                int code = e.getKeyCode();
+                System.out.println("   Char: " + e.getKeyChar());
+                           }
+            private String keyboardLocation(int keybrd) {
+                switch (keybrd) {
+                    case KeyEvent.KEY_LOCATION_RIGHT:
+                        return "Right";
+                    case KeyEvent.KEY_LOCATION_LEFT:
+                        return "Left";
+                    case KeyEvent.KEY_LOCATION_NUMPAD:
+                        return "NumPad";
+                    case KeyEvent.KEY_LOCATION_STANDARD:
+                        return "Standard";
+                    case KeyEvent.KEY_LOCATION_UNKNOWN:
+                    default:
+                        return "Unknown";
+                }
+            }
+        };
+        JTextField textField = new JTextField();
+        textField.addKeyListener(listener);
+        contentPane.add(textField, BorderLayout.NORTH);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
